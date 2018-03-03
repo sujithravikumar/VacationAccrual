735ec840627363931d43e0c28c2c95ce5de5a1f3
Sujith <sujithravikumar@gmail.com>
2018-03-03 01:14:11 -0800

    added javascript logic

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,37 @@
-﻿
+﻿$(document).ready(function(){
+    $("#periodsTbl td:nth-child(4)").each(function () {
+        if (parseInt($(this).text()) >= 120) {
+            $(this).addClass('red-yellow');
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#periodsTbl input").change(function(){
+        var accural;
+        var take;
+        var balance;
+        $("#periodsTbl tr").each(function (index) {
+            take = parseFloat($(this).find(".take").val());
+            if(index > 0) {
+                if(index == 1) {
+                    accural = parseFloat($(this).find(".accural").text());
+                    balance = parseFloat($(this).find(".balance").text());
+                    balance = balance - take;
+                }
+                else {
+                    balance = balance + accural - take;
+                }
+                $(this).find(".balance").text(balance.toFixed(2));
+            }
+        });
+        $("#periodsTbl td:nth-child(4)").each(function () {
+            if (parseInt($(this).text()) >= 120) {
+                $(this).addClass('red-yellow');
+            }
+            else {
+                $(this).removeClass('red-yellow');
+            }
+        });
+    });
+});
