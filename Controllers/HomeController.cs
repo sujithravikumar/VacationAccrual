92ecaa050510fea809b585a43fcdd6099023cf86
Sujith <sujithravikumar@gmail.com>
2018-03-04 01:44:05 -0800

    added Application Insights for Azure

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,16 +2,22 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using VacationAccrual.Models;
+using Microsoft.ApplicationInsights;
 
 namespace VacationAccrual.Controllers
 {
     public class HomeController : Controller
     {
+        private TelemetryClient telemetry = new TelemetryClient();
         public ActionResult Index(VacationAccrualViewModel vm)
         {
+            
             if(vm.SelectedStartDate != default(DateTime))
             {
                 vm.SetPeriodList(vm.SelectedStartDate, vm.SelectedPeriods, vm.Accural, vm.Balance);
+                
+                //Azure Application Insights
+                telemetry.TrackEvent("Calculated Vacation Accural");
             }
             return View(vm);
         }
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Vacation/PTO Accrual Calculator/Forecast
+# Vacation Accrual Forecast Calculator
 
-ASP.NET Core 2.0 MVC
+A calculator used for forecasting the vacation accrual for the pay periods. 
 
-Work in Progress...
+### Definitions
+
+* Current Pay Period Startdate (Unit: Date) - Startdate (Sunday) of the current pay period (2 weeks)
+* Max Balance Limit (Unit: Hours) - Maximum balance hours limit above which accrual will be forfeited
+* Pay Periods (Unit: Number) - Number of pay periods to be displayed for forecasting
+* Accrual (Unit: Hours) - Accural hours per pay period, for example: 4.62
+* Previous Pay Period Balance (Unit: Hours) - Balance hours of previous pay period, for example: 116.02
