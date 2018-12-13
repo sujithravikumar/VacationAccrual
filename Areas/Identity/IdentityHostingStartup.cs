cd7e07d9a04f4a862c0c5918a590452464c60044
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2018-12-12 21:34:16 -0800

    Scaffold identity register and login pages

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using vacation_accrual_buddy.Data;
+
+[assembly: HostingStartup(typeof(vacation_accrual_buddy.Areas.Identity.IdentityHostingStartup))]
+namespace vacation_accrual_buddy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
