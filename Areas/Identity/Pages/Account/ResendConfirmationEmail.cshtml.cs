317ec078d83e7b5f414ff29baa3cddfe58e13eee
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2018-12-14 22:57:34 -0800

    Implement resend confirmation email page

---
+++ Areas/Identity/Pages/Account/ResendConfirmationEmail.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vacation_accrual_buddy.Areas.Identity.Pages.Account
+{
+    public class ResendConfirmationEmailModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public ResendConfirmationEmailModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+                if (user == null)
+                {
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return RedirectToPage("./RegisterConfirmation");
+                }
+
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var callbackUrl = Url.Page(
+                    "/Account/ConfirmEmail",
+                    pageHandler: null,
+                    values: new { userId = user.Id, code = code },
+                    protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(
+                    Input.Email,
+                    "Thanks for Using Vacation Accrual Buddy! Please Confirm Your Email",
+                    $@"Hello {Input.Email}!<br><br>
+                    Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}' target='_blank'>clicking here</a>.<br><br>
+                    If you received this message but did not attempt to register, 
+                    you can safely disregard this email -- no further action is required.<br><br>
+                    Thank you,<br>
+                    The Vacation Accrual Buddy Team<br><br>
+                    Never forfeit a vacation day!"
+                );
+
+                return RedirectToPage("./RegisterConfirmation");
+            }
+
+            return Page();
+        }
+    }
+}
