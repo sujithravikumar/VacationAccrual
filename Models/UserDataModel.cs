cbd6d9369f21e2463eb1694149f6890db2633bfd
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-02 21:54:03 -0800

    Create User Data model

---
+++ Models/UserDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace vacation_accrual_buddy.Models
+{
+    public class UserDataModel
+    {
+        public bool Start_Date_Even_Ww { get; set; }
+        public decimal Accrual { get; set; }
+        public int Max_Balance { get; set; }
+        public int Period { get; set; }
+    }
+}
