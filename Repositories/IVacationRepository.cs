ae9ab5fa43309c10e4f94d50b17d2178fb8a955b
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-03 19:03:50 -0800

    Create Vacation Repository interface

---
+++ Repositories/IVacationRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using vacation_accrual_buddy.Models;
+namespace vacation_accrual_buddy.Repositories
+{
+    public interface IVacationRepository
+    {
+        VacationDataModel Get(
+            string userId,
+            DateTime startDate,
+            int period);
+
+        void Insert(
+            string userId,
+            DateTime startDate,
+            DateTime endDate,
+            decimal accrual,
+            decimal take,
+            decimal balance,
+            decimal forfeit);
+
+        void Update(
+            string userId,
+            DateTime startDate,
+            DateTime endDate,
+            decimal take,
+            decimal balance,
+            decimal forfeit);
+    }
+}
