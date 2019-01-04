cac8fd1327ad4ebd5adcc67e04c53aba832732a2
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-03 19:02:59 -0800

    Create VacationDataModel

---
+++ Models/VacationDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace vacation_accrual_buddy.Models
+{
+    public class VacationDataModel
+    {
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public decimal Accrual { get; set; }
+        public decimal Take { get; set; }
+        public decimal Balance { get; set; }
+        public decimal Forfeit { get; set; }
+    }
+}
