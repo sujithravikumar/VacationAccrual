83dbee7e6741b762b3598b2b81cb1f37bad571b3
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-05 13:10:58 -0800

    Add toastr library for notifications

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,6 +15,23 @@
         step: 0.01,
         numberFormat: "n"
     });
+    toastr.options = {
+      "closeButton": false,
+      "debug": false,
+      "newestOnTop": false,
+      "progressBar": false,
+      "positionClass": "toast-top-right",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "2000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    }
 });
 
 $(document).ready(function() {
