01c6f886b193bd2f327ce8e7219a47013efd93ed
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-17 19:21:23 -0800

    Add email alert properties to the model

--- Models/UserDataModel.cs
+++ Models/UserDataModel.cs
@@ -7,5 +7,7 @@
         public int Max_Balance { get; set; }
         public int Period { get; set; }
         public decimal Take_Days_Off { get; set; }
+        public bool Email_Alert_Enabled { get; set; }
+        public int Email_Alert_Days_Before { get; set; }
     }
 }
