0ca7b34858c8511f05b184df0f5f9de391e142ed
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-26 16:09:22 -0800

    Add privacy policy page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -289,6 +289,11 @@ namespace vacation_accrual_buddy.Controllers
             }
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
